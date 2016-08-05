94e958aa1a93f5717846b6b49bc3050eff7a3400
Mark van der Loo <mark.vanderloo@gmail.com>
2016-08-05 11:30:26 +0200

    added skylark dataset for examples

---
+++ pkg/R/skylark.R
@@ -0,0 +1,12 @@
+#' @name skylark
+#' @title Skylark population data
+#' 
+#' @description 
+#' 
+#' The Skylark dataset that was included with the original TRIM software.
+#' 
+#'
+#' @docType data
+#' @format \code{.RData}
+#'
+NULL
