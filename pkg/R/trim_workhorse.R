1cde5c4f38ad844a13b659cc8cc08f7a02f07fb6
Patrick Bogaart <pwbogaart@gmail.com>
2022-06-08 18:37:46 +0200

    fixed typo

--- pkg/R/trim_workhorse.R
+++ pkg/R/trim_workhorse.R
@@ -15,7 +15,7 @@
 #' @param conv_crit convergence criterion.
 #' @param max_iter maximum number of iterations allowed.
 #' @param max_beta maximum value for beta parameters
-#' @return a list of class \code{trim}, that contains all output, statistiscs, etc.
+#' @return a list of class \code{trim}, that contains all output, statistics, etc.
 #'   Usually this information is retrieved by a set of postprocessing functions
 #'
 #' @keywords internal
